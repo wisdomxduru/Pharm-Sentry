a4da07620a8f8a9118383cdeac6a9123d21b179e
otunaboEmmanuel <otunabochidubem@gmail.com>
2024-11-30 02:33:26 +0100

    changes

--- server/connect.js
+++ server/connect.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2'
 export const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"8597",
+    password:"",
     database:"pharma_sentry"
 });
 
